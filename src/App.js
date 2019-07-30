8eefe72ff0ac1cd2692d63fac0f252c9d823a18e
Leogan <leogan1399@gmail.com>
2019-07-30 15:41:16 -0500

    navigation updated using withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import {Route} from 'react-router-dom';
+
+const HatsPage = () =>(
+  <div>
+    <h1> Hats Page </h1>
+  </div>
+)
+
+
 
 function App() {
   return (
     <div>
-      <Homepage/>
+      <Route exact path='/' component={Homepage} />
+      <Route path='/shop/hats' component={HatsPage}/>
     </div>
   );
 }
