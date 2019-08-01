96aceef83876ea74a0d4e1357d116097cfa31094
Leogan <leogan1399@gmail.com>
2019-08-01 11:52:42 -0500

    page updated with shop page, two new components created, collection preview and collection item

--- src/App.js
+++ src/App.js
@@ -2,20 +2,14 @@ import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import {Route} from 'react-router-dom';
-
-const HatsPage = () =>(
-  <div>
-    <h1> Hats Page </h1>
-  </div>
-)
-
+import ShopPage from './pages/shop/shop.component';
 
 
 function App() {
   return (
     <div>
       <Route exact path='/' component={Homepage} />
-      <Route path='/shop/hats' component={HatsPage}/>
+      <Route path='/shop/hats' component={ShopPage}/>
     </div>
   );
 }
