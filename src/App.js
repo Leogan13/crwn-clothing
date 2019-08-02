61a84cd3d41fb5dfefb545e28fed3bab1f10b9df
Leogan <leogan1399@gmail.com>
2019-08-02 10:27:33 -0500

    sign in and sign out created, sign in component created, no functionality yet

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Homepage from './pages/homepage/homepage.component';
 import {Route} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+
+
 
 
 function App() {
@@ -12,6 +15,7 @@ function App() {
       <Header/>
       <Route exact path='/' component={Homepage} />
       <Route path='/shop' component={ShopPage}/>
+      <Route path='/signin' component={SignInAndSignUpPage}/>
     </div>
   );
 }
